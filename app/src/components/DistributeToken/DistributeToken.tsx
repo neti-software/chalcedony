f61ea6fa46315761a22c95acf049e5e4ca394f7e
lukasz-wal <143998006+lukasz-wal@users.noreply.github.com>
2023-09-29 11:17:16 +0200

    Add qr code generator (#15)

--- app/src/components/DistributeToken/DistributeToken.tsx
+++ app/src/components/DistributeToken/DistributeToken.tsx
@@ -19,6 +19,8 @@ import Box from "../Box";
 import CustomTokenLogo from "../CustomTokenLogo";
 import styles from "./DistributeToken.module.scss";
 import SymbolInput from "./SymbolInput";
+import QRCode from "qrcode";
+import DialogWhite from "../DialogWhite";
 
 const steps: Array<{
   step: string;
@@ -53,6 +55,7 @@ type EmailInput = {
 
 type QrCodeInput = {
   generated: boolean;
+  qrCode: string;
 };
 
 const DistributeToken: FC = () => {
@@ -67,10 +70,12 @@ const DistributeToken: FC = () => {
     { email: "" },
   ]);
   const [qrcodeInputs, setQrcodeInputs] = useState<Array<QrCodeInput>>([
-    { generated: false },
+    { generated: false, qrCode: "" },
   ]);
 
   const [tokensLeft, setTokensLeft] = useState("0");
+  const [showQrDialog, setShowQrDialog] = useState<boolean>(false);
+  const [qrCodeDialogData, setQrCodeDialogData] = useState<string>("");
 
   const [{ wallet }] = useConnectWallet();
 
@@ -88,7 +93,7 @@ const DistributeToken: FC = () => {
 
     for (let index = 0; index < value; index++) {
       newEmailInputs.push({ email: "" });
-      newQrcodeInputs.push({ generated: false });
+      newQrcodeInputs.push({ generated: false, qrCode: "" });
     }
 
     setEmailInputs(newEmailInputs);
@@ -121,11 +126,21 @@ const DistributeToken: FC = () => {
     setAmount(value);
   };
 
-  const generateSingleQrCode = (index: number) => {
+  const generateQR = async (value: string) => {
+    try {
+      const result = await QRCode.toDataURL(value);
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const generateSingleQrCode = async (index: number) => {
     if (qrcodeInputs[index].generated) return;
 
     const newItems = [...qrcodeInputs];
-    newItems[index] = { generated: true };
+    const qrCode = await generateQR("examples.com");
+    newItems[index] = { generated: true, qrCode };
     setQrcodeInputs(newItems);
   };
 
@@ -136,212 +151,234 @@ const DistributeToken: FC = () => {
     setDistributionInputs(1);
   };
 
+  const showQrCode = (index: number) => {
+    if (!qrcodeInputs[index].generated) return;
+
+    setQrCodeDialogData(qrcodeInputs[index].qrCode);
+
+    setShowQrDialog(true);
+  };
+
+  const closeQrCode = () => {
+    setShowQrDialog(false);
+    setQrCodeDialogData("");
+  };
+
   useEffect(() => {
     calcTokensLeft();
   // FIXME calcTokensLeft should probably be defined inside the useEffect or with useMemo and added to deps
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isTokenBalanceLoading, amount, selectedToken, wallet]);
 
-  // useEffect(() => {
-  //   debugger;
-  //   refetchTokenBalance();
-  // }, [wallet]);
-
   return (
-    <Box>
-      <div className={styles.title}>
-        <div>Distribute </div>
-        <div>
-          <span>Tokens as Vouchers</span>
+    <>
+      <DialogWhite isOpen={showQrDialog} handleClose={closeQrCode}>
+        <div className={styles.dialogQrCode}>
+          <img src={qrCodeDialogData} />
         </div>
-      </div>
-      {step === 1 ? (
-        <>
-          <div className={styles.middle}>
-            <div className={styles.text}>
-              With a few simple steps, you can create magic voucher links for
-              your users. Connect your wallet, select the token, deposit it into
-              our contract, and provide a list of email addresses. <br />
-              Your recipients will these magic links, or you can generate QR
-              codes leading to them. When users click the link, they're directed
-              to a collection page where they can claim their tokens instantly,
-              without the need to generate a wallet beforehand.
-              <br />
-              <span>Simplify token distribution with us today.</span>
-            </div>
-            <CustomTokenLogo />
+      </DialogWhite>
+      <Box>
+        <div className={styles.title}>
+          <div>Distribute </div>
+          <div>
+            <span>Tokens as Vouchers</span>
           </div>
-          <div className={styles.steps}>
-            {steps.map(({ step, text, image }, index) => {
-              return (
-                <div key={index} className={styles.singleStep}>
-                  <div className={styles.no}>
-                    <img src={step} />
-                  </div>
-                  <div className={styles.text}>{text}</div>
-                  <div className={styles.component}>
-                    <img src={image} />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles.button}>
-            <button disabled={!wallet} onClick={() => setStep(2)}>
-              start
-            </button>
-          </div>
-        </>
-      ) : (
-        <div className={styles.assets}>
-          <div className={styles.label}>Choose asset</div>
-          <div className={styles.assetList}>
-            {tokensList.map((tokenAddress, index) => {
-              return (
-                <div
-                  key={index}
-                  onClick={() => onChangeActiveToken(tokenAddress)}
-                  className={classNames(
-                    styles.singleAsset,
-                    tokenAddress === selectedToken
-                      ? styles.selected
-                      : styles.hide
-                  )}
-                >
-                  <div className={styles.logo}>
-                    <img src={ASSETS_ICONS[tokenAddress] ?? customAssetIcon} />
-                  </div>
-                  <div className={styles.symbol}>
-                    {!isTokenSymbolLoading && tokenSymbols
-                      ? tokenSymbols[tokenAddress]
-                      : "Loading symbol..."}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles.balance}>
-            <div className={styles.label}>Your balance:{""}</div>
-            {tokenBalances && !isTokenBalanceLoading ? (
-              <div className={styles.value}>{tokensLeft}</div>
-            ) : (
-              "Loading your'e balance..."
-            )}{" "}
-            {!isTokenSymbolLoading && tokenSymbols
-              ? tokenSymbols[selectedToken]
-              : ""}
-          </div>
-          <div className={styles.inputs}>
-            <div className={styles.amount}>
-              <div className={styles.label}>Amount</div>
-              <SymbolInput
-                value={amount}
-                symbol={
-                  !isTokenSymbolLoading && tokenSymbols
-                    ? tokenSymbols[selectedToken]
-                    : ""
-                }
-                onChange={changeAmountValue}
-              />
-            </div>
-            <div className={styles.split}>
-              <div className={styles.label}>Split</div>
-              <SymbolInput
-                value={split}
-                symbol="CODES"
-                min={1}
-                onChange={changeSplitValue}
-              />
-            </div>
-          </div>
-          <div className={styles.information}>
-            Each of the users will get: {split == 0 ? 0 : amount / split}{" "}
-            {tokenSymbols && !isTokenSymbolLoading
-              ? tokenSymbols[selectedToken]
-              : ""}
-          </div>
-          <div className={styles.label}>Distribution Channel</div>
-          <div className={styles.channels}>
-            <button
-              className={classNames(
-                styles.distChannel,
-                activeChannel === 1 ? styles.activeChannel : ""
-              )}
-              onClick={() => setActiveChannel(1)}
-            >
-              <img src={emailIcon} alt="email-channel" />
-            </button>
-            <button
-              className={classNames(
-                styles.distChannel,
-                activeChannel === 2 ? styles.activeChannel : ""
-              )}
-              onClick={() => setActiveChannel(2)}
-            >
-              <img src={qrIcon} alt="qr-code-channel" />
-            </button>
-          </div>
-          <div className={styles.dynamicInputs}>
-            {activeChannel === 1 ? (
-              <div className={styles.emails}>
-                {split > 0 ? (
-                  <>
-                    <div className={styles.generateButton}>
-                      <button>Import CSV</button>
-                    </div>
-                    <div className={styles.label}>Distribution e-Mails</div>
-                    {emailInputs.map((input, index) => {
-                      return (
-                        <div key={index} className={styles.emailItem}>
-                          <SymbolInput
-                            symbol={`User #${index + 1}`}
-                            value={input.email}
-                            placeholder="@mail"
-                          />
-                        </div>
-                      );
-                    })}
-                  </>
-                ) : null}
+        </div>
+        {step === 1 ? (
+          <>
+            <div className={styles.middle}>
+              <div className={styles.text}>
+                With a few simple steps, you can create magic voucher links for
+                your users. Connect your wallet, select the token, deposit it
+                into our contract, and provide a list of email addresses. <br />
+                Your recipients will these magic links, or you can generate QR
+                codes leading to them. When users click the link, they're
+                directed to a collection page where they can claim their tokens
+                instantly, without the need to generate a wallet beforehand.
+                <br />
+                <span>Simplify token distribution with us today.</span>
               </div>
-            ) : null}
-            {activeChannel === 2 ? (
-              <div className={styles.qrCodes}>
-                {split > 0 ? (
-                  <>
-                    <div className={styles.generateButton}>
-                      <button>Generate & Download All</button>
+              <CustomTokenLogo />
+            </div>
+            <div className={styles.steps}>
+              {steps.map(({ step, text, image }, index) => {
+                return (
+                  <div key={index} className={styles.singleStep}>
+                    <div className={styles.no}>
+                      <img src={step} />
                     </div>
-                    <div className={styles.label}>Distribution QR codes</div>
-                    {qrcodeInputs.map((input, index) => {
-                      return (
-                        <div className={styles.qrCodeItem} key={index}>
-                          <button
-                            className={classNames(
-                              styles.generateQrButton,
-                              input.generated ? styles.generatedQr : ""
-                            )}
-                            disabled={input.generated}
-                            onClick={() => generateSingleQrCode(index)}
-                          >
-                            Generate QR-Code
-                          </button>
-                          {input.generated ? (
-                            <button className={styles.showQrButton}>
-                              <img src={qrIcon} alt="show-qr" />
+                    <div className={styles.text}>{text}</div>
+                    <div className={styles.component}>
+                      <img src={image} />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className={styles.button}>
+              <button disabled={!wallet} onClick={() => setStep(2)}>
+                start
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={styles.assets}>
+            <div className={styles.label}>Choose asset</div>
+            <div className={styles.assetList}>
+              {tokensList.map((tokenAddress, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={() => onChangeActiveToken(tokenAddress)}
+                    className={classNames(
+                      styles.singleAsset,
+                      tokenAddress === selectedToken
+                        ? styles.selected
+                        : styles.hide
+                    )}
+                  >
+                    <div className={styles.logo}>
+                      <img
+                        src={ASSETS_ICONS[tokenAddress] ?? customAssetIcon}
+                      />
+                    </div>
+                    <div className={styles.symbol}>
+                      {!isTokenSymbolLoading && tokenSymbols
+                        ? tokenSymbols[tokenAddress]
+                        : "Loading symbol..."}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className={styles.balance}>
+              <div className={styles.label}>Your balance:{""}</div>
+              {tokenBalances && !isTokenBalanceLoading ? (
+                <div className={styles.value}>{tokensLeft}</div>
+              ) : (
+                "Loading your'e balance..."
+              )}{" "}
+              {!isTokenSymbolLoading && tokenSymbols
+                ? tokenSymbols[selectedToken]
+                : ""}
+            </div>
+            <div className={styles.inputs}>
+              <div className={styles.amount}>
+                <div className={styles.label}>Amount</div>
+                <SymbolInput
+                  value={amount}
+                  symbol={
+                    !isTokenSymbolLoading && tokenSymbols
+                      ? tokenSymbols[selectedToken]
+                      : ""
+                  }
+                  onChange={changeAmountValue}
+                />
+              </div>
+              <div className={styles.split}>
+                <div className={styles.label}>Split</div>
+                <SymbolInput
+                  value={split}
+                  symbol="CODES"
+                  min={1}
+                  onChange={changeSplitValue}
+                  disabled={true}
+                />
+              </div>
+            </div>
+            <div className={styles.information}>
+              Each of the users will get: {split == 0 ? 0 : amount / split}{" "}
+              {tokenSymbols && !isTokenSymbolLoading
+                ? tokenSymbols[selectedToken]
+                : ""}
+            </div>
+            <div className={styles.label}>Distribution Channel</div>
+            <div className={styles.channels}>
+              <button
+                className={classNames(
+                  styles.distChannel,
+                  activeChannel === 1 ? styles.activeChannel : ""
+                )}
+                disabled={true}
+                onClick={() => setActiveChannel(1)}
+              >
+                <img src={emailIcon} alt="email-channel" />
+              </button>
+              <button
+                className={classNames(
+                  styles.distChannel,
+                  activeChannel === 2 ? styles.activeChannel : ""
+                )}
+                onClick={() => setActiveChannel(2)}
+              >
+                <img src={qrIcon} alt="qr-code-channel" />
+              </button>
+            </div>
+            <div className={styles.dynamicInputs}>
+              {activeChannel === 1 ? (
+                <div className={styles.emails}>
+                  {split > 0 ? (
+                    <>
+                      <div className={styles.generateButton}>
+                        <button>Import CSV</button>
+                      </div>
+                      <div className={styles.label}>Distribution e-Mails</div>
+                      {emailInputs.map((input, index) => {
+                        return (
+                          <div key={index} className={styles.emailItem}>
+                            <SymbolInput
+                              symbol={`User #${index + 1}`}
+                              value={input.email}
+                              placeholder="@mail"
+                            />
+                          </div>
+                        );
+                      })}
+                    </>
+                  ) : null}
+                </div>
+              ) : null}
+              {activeChannel === 2 ? (
+                <div className={styles.qrCodes}>
+                  {split > 0 ? (
+                    <>
+                      <div className={styles.generateButton}>
+                        <button disabled>Generate & Download All</button>
+                      </div>
+                      <div className={styles.label}>Distribution QR codes</div>
+                      {qrcodeInputs.map((input, index) => {
+                        return (
+                          <div className={styles.qrCodeItem} key={index}>
+                            <button
+                              className={classNames(
+                                styles.generateQrButton,
+                                input.generated ? styles.generatedQr : ""
+                              )}
+                              disabled={input.generated}
+                              onClick={() => generateSingleQrCode(index)}
+                            >
+                              Generate QR-Code
                             </button>
-                          ) : null}
-                        </div>
-                      );
-                    })}
-                  </>
-                ) : null}
-              </div>
-            ) : null}
+                            {input.generated ? (
+                              <button
+                                className={styles.showQrButton}
+                                onClick={() => showQrCode(index)}
+                              >
+                                <img src={qrIcon} alt="show-qr" />
+                              </button>
+                            ) : null}
+                          </div>
+                        );
+                      })}
+                    </>
+                  ) : null}
+                </div>
+              ) : null}
+            </div>
           </div>
-        </div>
-      )}
-    </Box>
+        )}
+      </Box>
+    </>
   );
 };
 
